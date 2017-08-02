858d4ef4b1239ebe44bc7b04c33311b9d285aead
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-08-02 08:21:13 -0700

    update for class 3

---
+++ class-03-lists-loops-box_model/demo/script.js
@@ -0,0 +1,20 @@
+/* switching and looping fruits example */
+var favColor = prompt( 'what is your favorite color?' );
+// TODO depending on the color, reply with a fruit of that color
+
+
+
+
+/* polling commits by students example */
+// TODO prompt students asking how many commits they made yesterday
+var commits = [];
+
+/* use a for loop */
+
+
+/* use a do while loop */
+
+
+/* use a while loop */
+
+
